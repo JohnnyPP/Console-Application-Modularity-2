f764592f34925961b983b8c50359d43f1c4960e0
JohnnyPP <johnnnyppp@gmail.com>
2013-06-29 02:38:34 +0200

    Part 3
    http://youtu.be/KTtTW-BmXJg?t=18m40s

---
+++ CalculatorLibrary/CalculatorReplLoop.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modularity
+{
+    public class CalculatorReplLoop
+    {
+        public CalculatorReplLoop()
+        {
+            calculator = new Calculator();
+            inputParserService = new InputParserService();
+            inputService = new ConsoleInputService();
+            outputService = new ConsoleOutputService();
+        }
+
+        public void Run()
+        {
+            while (true)
+            {
+                string command = inputService.ReadCommand();
+
+                try
+                {
+                    CommandTypes commandType = inputParserService.ParseCommand(command);
+                    Arguments args = inputService.ReadArguments();
+                    outputService.WriteMessage(calculator.Execute(commandType, args).ToString());
+                }
+                catch
+                {
+
+                    outputService.WriteMessage("Mistake!");
+                }
+
+            }
+            
+
+        }
+        ConsoleInputService inputService;
+        ConsoleOutputService outputService;
+        Calculator calculator;
+        InputParserService inputParserService;
+    }
+}
