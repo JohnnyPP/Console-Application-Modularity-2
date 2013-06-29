3ccea89997d3fcead93ddd50b8be233caf1afba6
JohnnyPP <johnnnyppp@gmail.com>
2013-06-29 20:41:43 +0200

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InputOutputLibrary;
+
+namespace Modularity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CalculatorReplLoop loop = new CalculatorReplLoop();
+            loop.OutputService = new MsgBoxOutputService();
+            loop.Run();
+        }
+
+    }
+}
